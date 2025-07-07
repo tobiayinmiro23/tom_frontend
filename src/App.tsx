eae398ef59c548e477d6efa032633877e2fe6e57
Ayinmiro tobi <ayinmirotobi@gmail.com>
2025-07-07 23:27:09 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AllPhotos, Photo, Login } from './AllFiles'
 import './App.css'
+import { appContext } from './Context'
+import { useContext } from 'react';
 
 function App() {
+  const appData = useContext(appContext)
 
 
 
@@ -12,11 +15,9 @@ function App() {
         <Routes>
           <Route path='/' element={<Login />} />
           <Route path='/login' element={<Login />} />
-          <Route path='/photos' element={<AllPhotos />} />
-          {/* {appData?.username !== '' && <Route path='/photos' element={<AllPhotos />} />} */}
-          <Route path='/photo/:id' element={<Photo />} />
-          {/* {appData?.username !== '' && <Route path='/photo/:id' element={<Photo />} />} */}
-          <Route path='*' element={<h1>route unavailable</h1>} />
+          {appData?.username !== '' && <Route path='/photos' element={<AllPhotos />} />}
+          {appData?.username !== '' && <Route path='/photo/:id' element={<Photo />} />}
+          <Route path='*' element={<h1 className='text-center mt-[8rem]'>route unavailable</h1>} />
         </Routes>
       </Router>
 
