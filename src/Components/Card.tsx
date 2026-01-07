c4135632625d8bc9535d0be7b61dd54ba947d812
Ayinmiro tobi <ayinmirotobi@gmail.com>
2026-01-07 11:41:56 +0100

    Update Card.tsx

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -11,7 +11,7 @@ const Card = ({ result, page, setpage }: cardI) => {
     const timer = useCallback(() => {
         setTimeout(() => {
             setdisplayImageInfo(true)
-        }, 1160);
+        }, 760);
     }, [])
 
     timer()
@@ -41,18 +41,16 @@ const Card = ({ result, page, setpage }: cardI) => {
                                 <div className="bg-white p-2">
                                     <h3 className="font-bold text-sm truncate">
                                         <LinesEllipsis
-                                            text={item.user.username}
+                                            text={item?.user?.username}
                                             maxLine='1'
                                             ellipsis='...'
                                             trimRight
                                             basedOn='letters'
                                         />
-                                        {/* {item.user.username} */}
                                     </h3>
                                     <div className="text-sm line-clamp-2">
-                                        {/* {item.alt_description ?? item.description} */}
                                         <LinesEllipsis
-                                            text={item.alt_description ?? item.description}
+                                            text={item?.alt_description ?? item?.description}
                                             maxLine='1'
                                             ellipsis='...'
                                             trimRight
@@ -66,14 +64,17 @@ const Card = ({ result, page, setpage }: cardI) => {
                 ))}
             </div>
 
-            <div className='my-[2rem] text-center'>
-                {  // to determine wether to show the loading spinner animation or the load more button
-                    loadMore ?
-                        <div className="round animate-spin h-[1.5rem] w-[1.5rem] m-[auto] border-[3px] rounded-[50%]  border-x-[black] border-t-[black] border-b-[#ffffff] "></div>
-                        :
-                        <button type="submit" onClick={getMorePhotos} className="px-3 cursor-pointer py-1.5 rounded-md  text-[white]  bg-[black]  outline-none hover:bg-[#201e1e]" >load more photos </button>
-                }
-            </div>
+            {
+                result?.length !== null &&
+                <div className='my-[2rem] text-center'>
+                    {  // to determine wether to show the loading spinner animation or the load more button
+                        loadMore ?
+                            <div className="round animate-spin h-[1.5rem] w-[1.5rem] m-[auto] border-[3px] rounded-[50%]  border-x-[black] border-t-[black] border-b-[#ffffff] "></div>
+                            :
+                            <button type="submit" onClick={getMorePhotos} className="px-3 cursor-pointer py-1.5 rounded-md  text-[white]  bg-[black]  outline-none hover:bg-[#201e1e]" >load more photos </button>
+                    }
+                </div>
+            }
         </div>
     )
 }
